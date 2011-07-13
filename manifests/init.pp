710a76243c8c10629083643d5dbf3631195531c4
Kris Buytaert <sdog@inuits.be>
2011-07-13 15:15:13 +0200

    Adding debian support

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class vhosts {
 
 
 	file {
-		"/tmp/vhost":
-                	ensure => present;
+#		"/tmp/vhost":
+#                	ensure => present;
 		
 		 "/var/vhosts":
                         owner => root,
@@ -14,14 +14,16 @@ class vhosts {
         	}
 
 }
-
+# TODO Fix Removed for Debian 
 define vhosts::removed  ( $ip = "*", $port = "80" , $serveradmin = "root@localhost", $documentroot = '' , $servername = 'localhost' , $createroot = 'yes', $serveralias ) {
 
 
     	file { "/etc/httpd/vhosts.d/$servername.conf":
-		 	ensure => absent,	
-
-  		 	notify => Service["httpd"];
+  		name => $operatingsystem ? {
+                        /Debian|Ubuntu/ => '/etc/apache2/sites-enabled/$servername.conf',
+                        /Centos|Fedora/ => '/etc/httpd/vhosts.d/$servername.conf',
+                },
+		 	ensure => absent;
 	  	"/var/log/httpd/$servername/":
                         ensure => absent,
     			recurse => true, purge => true, force => true;
@@ -47,14 +49,38 @@ define vhosts::removed  ( $ip = "*", $port = "80" , $serveradmin = "root@localho
 
 define vhosts::host  ( $ip = "*", $port = "80" , $serveradmin = "root@localhost", $documentroot = '' , $servername = 'localhost' , $createroot = 'yes', $serveralias ) {
 
+  if  ($operatingsystem in ['Debian', 'Ubuntu']) 
+                { $baselogdir = "/var/log/apache2" 
+                  $apacheuser = "root"
+                  $apachegroup = "root" 
+		
+
+		file {"000-$servername":
+			name => "/etc/apache2/sites-enabled/000-$servername",
+			target => "/etc/apache2/sites-available/$servername",
+			ensure =>  "link";
+			}
+
+		 }
+        if  ($operatingsystem in ['Centos', 'Fedora']) 
+                { $baselogdir = "/var/log/httpd" 
+                  $apacheuser = "apache" 
+                  $apachegroup = "apache" }
+
+
+
 
     	file { "/etc/httpd/vhosts.d/$servername.conf":
+		name => $operatingsystem ? {
+			/Debian|Ubuntu/ => "/etc/apache2/sites-available/$servername",
+			/Centos|Fedora/ => "/etc/httpd/vhosts.d/$servername.conf",
+		},
        		 mode    => 0644,
-  		 notify => Service["httpd"],
+  	#	 notify => Service["httpd"],
         	 content => template("vhosts/vhost.conf.erb");
 		}
-
-
+	
+      
 	# Create directories here 
 
 	if $createroot == 'yes' {
@@ -66,25 +92,30 @@ define vhosts::host  ( $ip = "*", $port = "80" , $serveradmin = "root@localhost"
 			
 		
   		file { "$documentroot":
-               	  owner => apache,
-                  group => apache,
+               	  owner => $apacheuser,
+                  group => $apachegroup,
                   ensure => directory ,
                   recurse => true;
         	}
 
 	}		
+	
+
+
+		
+
 
 	file { 
-		"/var/log/httpd/$servername/":
+		"$baselogdir/$servername/":
 			ensure => directory,
 			recurse => true,
-			owner => apache,
-			group => apache;
-		"/var/log/httpd/$servername/access.log":
-			group => apache,
-			owner => apache;
-		"/var/log/httpd/$servername/error.log":
-			group => apache,
-			owner => apache;
+			owner => $apacheuser,
+			group => $apachegroup;
+		"$baselogdir/$servername/access.log":
+			group => $apacheugroup,
+			owner => $apacheuser;
+		"$baselogdir/$servername/error.log":
+			group => $apachegroup,
+			owner => $apacheuser;
 	}
 }
